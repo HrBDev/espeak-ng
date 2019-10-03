f84304eb287bb19345474538b9d99afa2c258b40
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2019-10-04 01:01:08 +0330

    + 64bit abi(s)

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,4 +1,4 @@
 APP_OPTIM             := release
 APP_PLATFORM          := android-26
 APP_TOOLCHAIN_VERSION := 4.6.3
-APP_ABI               := armeabi-v7a x86
+APP_ABI               := armeabi-v7a x86 arm64-v8a x86_64
